b7b8335b3d52bc17cb6b958d458232dbbd68f50c
ehabalshrida <ehabshridah@gmail.com>
2021-11-08 23:45:00 +0200

    done

---
+++ src/auth/routes.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const express = require('express');
+const authRouter = express.Router();
+const bcrypt=require('bcrypt')
+
+const { users } = require('./models/index.js');
+const basicAuth = require('./middleware/basic.js')
+const bearerAuth = require('./middleware/bearer.js')
+
+authRouter.post('/signup', async (req, res, next) => {
+  try {
+    // req.body.password = await bcrypt.hash(req.body.password, 5);
+
+    let userRecord = await users.create(req.body);
+    console.log(userRecord)
+    const output = {
+      user: userRecord,
+      token: userRecord.token
+    };
+    res.status(200).json(output);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+authRouter.post('/signin', basicAuth, (req, res, next) => {
+  const user = {
+    user: request.user,
+    token: request.user.token
+  };
+  console.log(user)
+  res.status(200).json(user);
+});
+
+authRouter.get('/users', bearerAuth, async (req, res, next) => {
+  const user = await users.findAll({});
+  const list = user.map(user => user.username);
+  res.status(200).json(list);
+});
+
+authRouter.get('/secret', bearerAuth, async (req, res, next) => {
+  res.status(200).send("Welcome to the secret area!")
+});
+
+
+module.exports = authRouter;
